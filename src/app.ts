6efe9541343d63bd956cf6437d99f942e6f4009a
ramansh18 <123497240+ramansh18@users.noreply.github.com>
2025-10-19 20:43:47 +0530

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -6,9 +6,11 @@ const app = express();
 
 // Allow requests from frontend
 app.use(cors({
-  origin: 'http://localhost:5173', // your frontend URL
-  methods: ['GET','POST','PUT','DELETE'],
-  credentials: true // if you need cookies/auth
+  origin: [
+    'http://localhost:5173', // for local dev
+    'https://cybernauts-frontend-igbl.vercel.app' // deployed frontend
+  ],
+  credentials: true
 }));
 app.use(express.json());
 
